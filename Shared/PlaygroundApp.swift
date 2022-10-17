bfc14d39d1a62d7647111b4a0b51c27e6f0cf2ac
smbrmoyo <brianmoyou@gmail.com>
2022-10-17 13:38:53 +0200

    Initial Commit

---
+++ Shared/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Shared
+//
+//  Created by Brian Moyou on 17.10.22.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
